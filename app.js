d9cf33bbcf2b1890c759606fd4a8261f090a1c94
Cairo Vieira <cairovieira@MacBook-Pro-de-Cairo.local>
2020-03-15 14:44:37 -0300

    quantitativas

--- app.js
+++ app.js
@@ -10,15 +10,18 @@ app.get("/health", (req, res) => {
 })
 
 app.post("/descritiva", (req, res) => {
-    let body = req.body
-    let dados = body.dados.split(";")
+    let body = req.body;
+    let dados = body.dados.toLowerCase().split(";");
+    dados.sort();
     let json = {
-        tipo: "qualitativa",
+        tipo: "",
         variavel: body.nomeVariavel,
         dados: []
-    }
+    };
+
     if (isNaN(dados[0])) {
         //Se não for um número, será qualitativa
+        json.tipo = "qualitativa"
         let dadosDistintos = [...new Set(dados)]
         dadosDistintos.forEach(element => {
             let vet = dados.filter(item => element == item)
@@ -27,17 +30,64 @@ app.post("/descritiva", (req, res) => {
             let obj = {
                 name: element,
                 value: vet.length,
-                fi: Math.round((100*repet)/total)
+                fi: Math.round((100 * repet) / total)
             }
             json.dados.push(obj)
         });
     }
     else {
         //Se for um número, será quantitativa
+        let vetAux = [];
+        dados.forEach(element => {
+            vetAux.push(Number(element))
+        });
+        dados = vetAux;
+        let dadosDistintos = [...new Set(dados)];
+        if (dadosDistintos.length <= 6) {
+            // Será quantitativa discreta
+            json.tipo = "quantitativaDiscreta"
+            dadosDistintos.forEach(element => {
+                let vet = dados.filter(item => element == item)
+                let total = dados.length
+                let repet = vet.length
+                let obj = {
+                    name: element,
+                    value: vet.length,
+                    fi: Math.round((100 * repet) / total)
+                }
+                json.dados.push(obj)
+            });
+        }
+        else {
+            //será quantitativa contínua
+            json.tipo = "quantitativaContinua"
+            let menor = dados[0];
+            let maior = dados[dados.length - 1];
+            let amplitude = maior - menor;
+            let constante = Math.round(1 + 3.3 * Math.log10(dados.length));
+            let intervaloClasses = Math.ceil(amplitude / constante)
+
+            for (let i = 0; i < constante; i++) {
+                let contador = 0
+                dados.forEach(element => {
+                    if (element >= menor && element < (menor + intervaloClasses)) {
+                        contador += 1
+                    }
+                })
+                let intervalo = menor + " |-- " + (menor + intervaloClasses);
+                menor = intervaloClasses + menor
+                let obj = {
+                    name: intervalo,
+                    value: contador,
+                    fi: Math.round((100 * contador) / dados.length)
+                }
+                json.dados.push(obj)
+            }
+        }
     }
     res.send(json)
 })
-                             
+
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log('API Granger online na porta %s', port);
