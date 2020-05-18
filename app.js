902b89ae24c27d910d3f8b67dd8c0162d3fab5b8
CairoVieira <vieira.cairo@hotmail.com>
2020-05-17 21:11:18 -0300

    Versão nova

--- app.js
+++ app.js
@@ -1,108 +1,128 @@
 var express = require("express");
 var cors = require("cors");
 var app = express();
-var { quickSort } = require("./sorting");
+var { quickSort, media, moda, mediana } = require("./sorting");
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/health", (req, res) => {
-      res.send("API Granger Online");
+	res.send("API Granger Online");
+});
+
+app.post("/mediana", (req, res) => {
+	let body = req.body;
+	let resultado = "";
+	resultado = mediana(body.tipo, body.dados);
+	res.send(resultado);
+});
+
+app.post("/quartil/:id", (req, res) => {
+	let body = req.body;
+	let quartil = req.params.id;
+	console.log("req", quartil, body);
+	res.send("teste");
 });
 
 app.post("/descritiva", (req, res) => {
-      let body = req.body;
-      body.dados = body.dados.toLowerCase().split(",").join(".");
-      let dados = body.dados.split(";");
-      let vetAux = [];
-      dados.forEach((element) => {
-            if (isNaN(element)) {
-                  vetAux.push(element.trim());
-            } else {
-                  vetAux.push(Number(element));
-            }
-      });
-      dados = vetAux;
-      quickSort(dados);
+	let body = req.body;
+	body.dados = body.dados.toLowerCase().split(",").join(".");
+	let dados = body.dados.split(";");
+	let vetAux = [];
+	dados.forEach((element) => {
+		if (isNaN(element)) {
+			vetAux.push(element.trim());
+		} else {
+			vetAux.push(Number(element));
+		}
+	});
+	dados = vetAux;
+	quickSort(dados);
 
-      let json = {
-            tipo: "",
-            variavel: body.nomeVariavel,
-            dados: [],
-      };
+	let json = {
+		tipo: "",
+		media: "",
+		moda: "",
+		mediana: "",
+		variavel: body.nomeVariavel,
+		dados: [],
+	};
 
-      if (isNaN(dados[0])) {
-            //Se não for um número, será qualitativa
-            json.tipo = "qualitativa";
-            let dadosDistintos = [...new Set(dados)];
-            let i = 0;
-            dadosDistintos.forEach((element) => {
-                  let vet = dados.filter((item) => element == item);
-                  let total = dados.length;
-                  let repet = vet.length;
-                  let obj = {
-                        id: i,
-                        name: element,
-                        value: vet.length,
-                        fi: Math.round((100 * repet) / total),
-                  };
-                  json.dados.push(obj);
-                  i++;
-            });
-      } else {
-            //Se for um número, será quantitativa
-            let dadosDistintos = [...new Set(dados)];
-            if (dadosDistintos.length <= 6) {
-                  // Será quantitativa discreta
-                  json.tipo = "quantitativaDiscreta";
-                  dadosDistintos.forEach((element) => {
-                        let vet = dados.filter((item) => element == item);
-                        let total = dados.length;
-                        let repet = vet.length;
-                        let obj = {
-                              name: element,
-                              value: vet.length,
-                              fi: Math.round((100 * repet) / total),
-                        };
-                        json.dados.push(obj);
-                  });
-            } else {
-                  //será quantitativa contínua
-                  json.tipo = "quantitativaContinua";
-                  let menor = dados[0];
-                  let maior = dados[dados.length - 1];
-                  let amplitude = maior - menor;
-                  let constante = Math.round(1 + 3.3 * Math.log10(dados.length));
-                  let intervaloClasses = Math.ceil(amplitude / constante);
+	if (isNaN(dados[0])) {
+		//Se não for um número, será qualitativa
+		json.tipo = "qualitativa";
+		let dadosDistintos = [...new Set(dados)];
 
-                  console.log("const", constante);
-                  console.log("amplitude", amplitude);
-                  console.log("intervaloClasses", intervaloClasses);
-                  console.log("ampldadositude", dados);
+		dadosDistintos.forEach((element) => {
+			let vet = dados.filter((item) => element == item);
+			let total = dados.length;
+			let repet = vet.length;
+			let obj = {
+				name: element,
+				value: vet.length,
+				fr: Math.round((100 * repet) / total),
+			};
+			json.dados.push(obj);
+		});
 
-                  for (let i = 0; i < constante; i++) {
-                        let contador = 0;
-                        dados.forEach((element) => {
-                              if (element >= menor && element < menor + intervaloClasses) {
-                                    contador += 1;
-                              }
-                        });
-                        let intervalo = menor + " |-- " + (menor + intervaloClasses);
-                        menor = intervaloClasses + menor;
-                        let obj = {
-                              name: intervalo,
-                              value: contador,
-                              fi: Math.round((100 * contador) / dados.length),
-                        };
-                        json.dados.push(obj);
-                  }
-            }
-      }
-      console.log("json", json);
-      res.send(json);
+		json.media = media(json.tipo).trim();
+		json.moda = moda(json.tipo, json.dados).trim();
+		json.mediana = mediana(json.tipo, dados).trim();
+	} else {
+		//Se for um número, será quantitativa
+		let dadosDistintos = [...new Set(dados)];
+		if (dadosDistintos.length <= 6) {
+			// Será quantitativa discreta
+			json.tipo = "quantitativaDiscreta";
+			dadosDistintos.forEach((element) => {
+				let vet = dados.filter((item) => element == item);
+				let total = dados.length;
+				let repet = vet.length;
+				let obj = {
+					name: element,
+					value: vet.length,
+					fr: Math.round((100 * repet) / total),
+				};
+				json.dados.push(obj);
+			});
+			json.media = media(json.tipo, json.dados).trim();
+			json.moda = moda(json.tipo, json.dados).trim();
+			json.mediana = mediana(json.tipo, dados).trim();
+		} else {
+			//será quantitativa contínua
+			json.tipo = "quantitativaContinua";
+			let menor = dados[0];
+			let maior = dados[dados.length - 1];
+			let amplitude = maior - menor;
+			let constante = Math.round(1 + 3.3 * Math.log10(dados.length));
+			let intervaloClasses = Math.ceil(amplitude / constante);
+
+			for (let i = 0; i < constante; i++) {
+				let contador = 0;
+				dados.forEach((element) => {
+					if (element >= menor && element < menor + intervaloClasses) {
+						contador += 1;
+					}
+				});
+				let intervalo = menor + " |-- " + (menor + intervaloClasses);
+				menor = intervaloClasses + menor;
+				let obj = {
+					name: intervalo,
+					value: contador,
+					fr: Math.round((100 * contador) / dados.length),
+				};
+				json.dados.push(obj);
+			}
+			json.media = media(json.tipo, json.dados).trim();
+			json.moda = moda(json.tipo, json.dados).trim();
+			json.mediana = mediana(json.tipo, json.dados).trim();
+		}
+	}
+	console.log("json", json);
+	res.send(json);
 });
 
-var port = process.env.PORT || 3030;
+var port = process.env.PORT || 3001;
 app.listen(port, function () {
-      console.log("API Granger online na porta %s", port);
+	console.log("API Granger online na porta %s", port);
 });
--- sorting.js
+++ sorting.js
@@ -1,39 +1,125 @@
 function troca(vet, i, j) {
-    let aux = vet[i]
-    vet[i] = vet[j]
-    vet[j] = aux
+	let aux = vet[i];
+	vet[i] = vet[j];
+	vet[j] = aux;
 }
 
 function quickSort(vet, posIni = 0, posFim = vet.length - 1) {
-    if (posFim > posIni) {
-        const posPivot = posFim
-        let posDiv = posIni - 1
-        for (let i = posIni; i < posFim; i++) {
-            if (vet[i] < vet[posPivot] && i != posDiv) {
-                posDiv++
-                troca(vet, i, posDiv)
-            }
-        }
-        posDiv++
-        troca(vet, posDiv, posPivot)
+	if (posFim > posIni) {
+		const posPivot = posFim;
+		let posDiv = posIni - 1;
+		for (let i = posIni; i < posFim; i++) {
+			if (vet[i] < vet[posPivot] && i != posDiv) {
+				posDiv++;
+				troca(vet, i, posDiv);
+			}
+		}
+		posDiv++;
+		troca(vet, posDiv, posPivot);
 
-        quickSort(vet, posIni, posDiv - 1)
+		quickSort(vet, posIni, posDiv - 1);
 
-        quickSort(vet, posDiv + 1, posFim)
-    }
+		quickSort(vet, posDiv + 1, posFim);
+	}
 }
 
-function media() {
-
+function media(tipo, dados) {
+	let media = 0;
+	let total = 0;
+	if (tipo !== "qualitativa") {
+		if (tipo === "quantitativaDiscreta") {
+			dados.forEach((element) => {
+				media += Number(element.name) * Number(element.value);
+				total += Number(element.value);
+			});
+			return "Média: " + (media / total).toFixed(2);
+		} else {
+			dados.forEach((element) => {
+				let xi = element.name.split("|--");
+				media += ((Number(xi[0]) + Number(xi[1])) / 2) * element.value;
+				total += element.value;
+			});
+		}
+		return "Média: " + (media / total).toFixed(2);
+	}
+	return "";
 }
 
-function moda(){
-
+function moda(tipo, dados) {
+	let moda = "";
+	if (tipo === "quantitativaContinua") {
+		let valor = -1;
+		dados.forEach((element) => {
+			let xi = element.name.split("|--");
+			if (element.value > valor) {
+				valor = element.value;
+				moda = ((Number(xi[0]) + Number(xi[1])) / 2).toFixed(2).toString();
+			}
+			if (element.value == valor) {
+				if (!moda.includes(((Number(xi[0]) + Number(xi[1])) / 2).toFixed(2).toString())) {
+					moda = moda + ", " + ((Number(xi[0]) + Number(xi[1])) / 2).toFixed(2).toString();
+				}
+			}
+		});
+	} else {
+		let valor = -1;
+		dados.forEach((element) => {
+			if (element.value > valor) {
+				valor = element.value;
+				moda = element.name.toString();
+			}
+			if (element.value == valor) {
+				if (!moda.includes(element.name.toString())) {
+					moda = moda + ", " + element.name;
+				}
+			}
+		});
+		let vetorModa = moda.split(",");
+		if (vetorModa.length == dados.length) return "Moda: não existe";
+	}
+	return "Moda: " + moda;
 }
 
-function mediana(){
+function mediana(tipo, dados) {
+	if (tipo != "quantitativaContinua") {
+		if (dados.length % 2 == 0) {
+			let indice = dados.length / 2;
+			if (dados[indice] == dados[indice - 1]) return "Mediana: " + dados[indice];
+			return "Mediana: " + dados[indice - 1] + ", " + dados[indice];
+		} else {
+			let indice = Math.floor(dados.length / 2);
+			return "Mediana: " + dados[indice];
+		}
+	} else {
+		let total = 0;
+		dados.forEach((element) => {
+			total += Number(element.value);
+		});
+		let posicao = total / 2;
+		let intervaloClasse = {};
+		let Fac = 0;
+		total = 0;
 
+		for (let index = 0; index < dados.length; index++) {
+			if (posicao >= total && posicao <= total + Number(dados[index].value)) {
+				intervaloClasse = dados[index];
+				break;
+			} else {
+				Fac += Number(dados[index].value);
+				total += Number(dados[index].value);
+			}
+		}
+		let limites = intervaloClasse.name.split("|--");
+		let mediana = Number(limites[0]) + ((posicao - Fac) / intervaloClasse.value) * Number(limites[1] - Number(limites[0]));
+		return "Mediana: " + mediana.toFixed(2).toString();
+	}
 }
 
-module.exports.quickSort = quickSort
+// function medidasSeparatriz(tipo, dados, divisor)
 
+module.exports = {
+	quickSort,
+	media,
+	moda,
+	mediana,
+};
