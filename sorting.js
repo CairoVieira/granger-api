908e738aa71d62033862c5a8087de9ee2aab2c15
CairoVieira <vieira.cairo@hotmail.com>
2020-05-19 21:25:57 -0300

    Medida separatriz continua

--- sorting.js
+++ sorting.js
@@ -115,27 +115,28 @@ function mediana(tipo, dados) {
 }
 
 function medidasSeparatriz(tipo, dados, medida, parte) {
-	if (tipo != "quantitativaContinua") {
-		let total = 0;
-		dados.forEach((item) => {
-			total += item.value;
-		});
-		let porcetagem = (100 / medida) * parte;
-		let posicao = Math.round((porcetagem * total) / 100);
-		let Fac = 0;
-		let intervaloClasse = {};
-		for (let index = 0; index < dados.length; index++) {
-			if (posicao >= Fac && posicao <= Fac + Number(dados[index].value)) {
-				intervaloClasse = dados[index];
-				break;
-			}
-			Fac += Number(dados[index].value);
+	let total = 0;
+	dados.forEach((item) => {
+		total += item.value;
+	});
+	let porcetagem = (100 / medida) * parte;
+	let posicao = Math.round((porcetagem * total) / 100);
+	let Fac = 0;
+	let intervaloClasse = {};
+	for (let index = 0; index < dados.length; index++) {
+		if (posicao >= Fac && posicao <= Fac + Number(dados[index].value)) {
+			intervaloClasse = dados[index];
+			break;
 		}
-		return "Medida Separatriz: " + intervaloClasse.name;
+		Fac += Number(dados[index].value);
 	}
-	if (tipo != "quantitativaContinua") {
-		return "teste";
+
+	if (tipo == "quantitativaContinua") {
+		let limites = intervaloClasse.name.split("|--");
+		let resultado = Number(limites[0]) + ((posicao - Fac) / intervaloClasse.value) * Number(limites[1] - Number(limites[0]));
+		return "Medida Separatriz: " + resultado.toFixed(2);
 	}
+	return "Medida Separatriz: " + intervaloClasse.name;
 }
 
 module.exports = {
